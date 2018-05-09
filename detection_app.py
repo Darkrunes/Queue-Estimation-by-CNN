767f22e3ac36faa2f00d6db27412a41f4104795a
Saffat Akanda <jadenandyubel@hotmail.com>
2018-05-09 17:12:54 +1000

    Initial Comit

---
+++ detection_app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# OpenCV Code to interact with Video/Image Streams go here
+# Code to draw boxes returned by YOLO should also go here
+
+# Saffat Shams Akanda, <Your Name Here>, <Your Name Here>
+
+import cv2
+import numpy as np
+import os
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ yolo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Class code for YOLO
+
+# Saffat Shams Akanda, <Your Name Here>, <Your Name Here>
+
+import keras
+import cv2
+import numpy as np
+import os
+
+
+class YOLO:
+
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def train(self):
+        pass
+
+    def build_yolo_model(self):
+        pass
+
+    def build_tiny_yolo_mode(self):
+        pass
